02564351e9b7fde9dd357ced60b315e9e5902900
reggev <reggev.am@gmail.com>
2020-10-19 13:06:13 +0300

    added docker compose and setup files

---
+++ scripts/mongo.setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# NOTE: the initial user HAS to be created on a primary node using localhost!
+# mongo-0.mongo has a higher priority so it'll be the primary on initial boot
+mongo <<EOF
+   var cfg = {
+        "_id": "rs",
+        "version": 1,
+        "members": [
+            {
+                "_id": 0,
+                "host": "mongo-0.mongo:27017",
+                "priority": 2
+            },
+            {
+                "_id": 1,
+                "host": "mongo-1.mongo:27017",
+                "priority": 0
+            },
+            {
+                "_id": 2,
+                "host": "mongo-2.mongo:27017",
+                "priority": 0
+            }
+        ]
+    };
+    rs.initiate(cfg, { force: true });
+    //rs.reconfig(cfg, { force: true });
+    rs.status();
+EOF
+sleep 10
+
+mongo <<EOF
+   use admin;
+   admin = db.getSiblingDB("admin");
+   admin.createUser(
+     {
+	user: "admin",
+        pwd: "password",
+        roles: [ { role: "root", db: "admin" } ]
+     });
+     db.getSiblingDB("admin").auth("admin", "password");
+     rs.status();
+EOF
