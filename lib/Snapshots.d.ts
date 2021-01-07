b698ed368cae04c18cf8b6aeaffc5c3d3bf7be26
reggev <reggev.am@gmail.com>
2021-01-07 11:09:54 +0200

    added snapshots results caching

--- lib/Snapshots.d.ts
+++ lib/Snapshots.d.ts
@@ -1,3 +1,4 @@
+import { FileMeta } from './DataSource';
 import { DataSource } from './DataSource.d';
 import { Id } from './MongoDB/types';
 import Collection from './MongoDB/Collection';
@@ -6,20 +7,24 @@ type Snapshot = {
     id?: Id;
     name: string;
     query: string;
+    filteredFilesList?: FileMeta[];
+    droppedFiles?: FileMeta[];
     dataSource: { id: string; name: string };
 };
 
-export type ResolvedSnapshot = {
-    id: Id;
-    name: string;
-    query: string;
+export type ResolvedSnapshot = Snapshot & {
     dataSource: DataSource;
 };
 
-export interface SnapshotsCollection extends Collection<Snapshot>, SnapshotOverrides {
+export interface SnapshotsCollection extends Collection<Snapshot> {
     create(params: Snapshot): Promise<Snapshot>;
     fetchDataSource(params: {
         snapshotName: string;
         dataSourceName: string;
     }): Promise<ResolvedSnapshot>;
+    updateFilesList(props: {
+        id: string;
+        filesList: FileMeta[];
+        droppedFiles: FileMeta[];
+    }): Promise<boolean>;
 }
