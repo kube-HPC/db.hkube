fc5f5a44343af0f96d8f95828de78c93572e4e33
Yehiyam Livneh <yehiyam@gmail.com>
2022-01-09 13:22:57 +0200

    fix script

--- scripts/startMongo.sh
+++ scripts/startMongo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 docker rm -f mongodb
-docker run -d -it --name mongodb \
+docker run -d --name mongodb \
       -p 27017:27017 \
       -e MONGO_INITDB_ROOT_USERNAME=tester \
       -e MONGO_INITDB_ROOT_PASSWORD=password mongo:4.4.1-bionic --replSet rs0
 
-INIT=$(docker exec -it mongodb sh -c 'echo "rs.initiate().ok || rs.status().ok"|mongo -u tester -p password --quiet' | tr -d '\r')
+INIT=$(docker exec mongodb sh -c 'echo "rs.initiate().ok || rs.status().ok"|mongo -u tester -p password --quiet' | tr -d '\r')
 COUNT=1
 MAX_RETRY=10
 while [ "$INIT" != "1" ]; do
@@ -15,7 +15,7 @@ while [ "$INIT" != "1" ]; do
   fi
   echo "Waiting for mongo to be ready... ($COUNT/$MAX_RETRY)"
   sleep 1
-  INIT=$(docker exec -it mongodb sh -c 'echo "rs.initiate().ok || rs.status().ok"|mongo -u tester -p password --quiet' | tr -d '\r')
+  INIT=$(docker exec mongodb sh -c 'echo "rs.initiate().ok || rs.status().ok"|mongo -u tester -p password --quiet' | tr -d '\r')
   COUNT=$((COUNT+1))
 done
 echo "Mongo is ready!"
